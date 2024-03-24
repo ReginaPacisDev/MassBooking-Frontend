21289c5904493d9ac34245ebf34d9cf8a2422c09
ReginaPacisDev <reginapacisdev@gmail.com>
2024-03-24 14:48:59 +0100

    fix: Add logic to redirect to dashboard if the token is still valid

--- src/containers/Admin/index.jsx
+++ src/containers/Admin/index.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
+import jwt from "jwt-decode";
 import { useNavigate, Outlet, useLocation } from "react-router-dom";
+import { ADMIN_ACCESS_TOKEN } from "../../helpers";
 
 const BASE_ADMIN_URL = "/admin";
 
@@ -9,7 +11,20 @@ const Admin = () => {
 
   useEffect(() => {
     if (location.pathname === BASE_ADMIN_URL) {
-      navigate(`${BASE_ADMIN_URL}/signin`);
+      const token = localStorage.getItem(ADMIN_ACCESS_TOKEN);
+
+      if (!token) {
+        navigate(`${BASE_ADMIN_URL}/login`);
+      }
+
+      const decodedToken = jwt(token);
+      const currentDate = new Date();
+
+      if (decodedToken.exp * 1000 > currentDate.getTime()) {
+        navigate(`${BASE_ADMIN_URL}/dashboard`);
+      } else {
+        navigate(`${BASE_ADMIN_URL}/login`);
+      }
     }
   }, [navigate, location]);
 
